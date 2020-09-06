73ac51376bb5595652a01300efdfce0d4128b38e
Christina Kiesz <ckiesz@gmail.com>
2020-09-06 16:55:58 -0700

    controller testing

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
-
+  describe '#main' do
+    it 'returns the main page' do
+      get "/"
+      expect(response).to render_template(:main)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
