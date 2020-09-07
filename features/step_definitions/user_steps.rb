b3e64c46500ff8a31aa046846a3cd65828652cb3
Christina Kiesz <ckiesz@gmail.com>
2020-09-07 16:08:43 -0700

    user steps skel

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,48 +1,49 @@
 Given('I am currently logged out') do
-  visit(root_path)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given('there is a {string} link in the header') do |string|
-  find('header').find_link(string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When('I click the {string} link in the header') do |string|
-  find('header').click_link(string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then('I should be taken to the {string} form') do |string|
-  find_field(id: string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then('it should have the content {string}') do |string|
-  expect(page).to have_content(string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then('it should have a form input for {string}') do |string|
-  find_field(string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then('it should have a button to {string}') do |string|
-  find_button(string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given('I am viewing the {string} form') do |string|
-  find_field(id: string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given('my name is {string}') do |string|
-  larry = User.new(name: string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given('my email is {string}') do |string|
-  larry.email = string
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When('I fill in the form input for {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When('I click the {string} button') do |string|
-  click_button(string)
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then('my user info should be saved in the db') do
@@ -96,3 +97,4 @@ end
 Then('I should be succesfully signed in') do
   pending # Write code here that turns the phrase above into concrete actions
 end
+
