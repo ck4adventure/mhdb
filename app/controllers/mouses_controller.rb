ad3c47b0c14088f7b43111a89ff6e5a8c94e7e3e
Christina Kiesz <ckiesz@gmail.com>
2020-09-08 16:53:13 -0700

    template fixes

--- app/controllers/mouses_controller.rb
+++ app/controllers/mouses_controller.rb
@@ -8,21 +8,39 @@ class MousesController < ApplicationController
   end
 
   def new
-  
+    
   end
 
   def create
-  
+    @mouse = Mouse.new(mouse_params)
+    if @mouse.save
+      redirect_to mouse_path(@mouse)
+    else
+      flash.now[:errors] = @mouse.errors.full_messages
+    end
   end
   def edit
-  
+    @mouse = Mouse.find(params[:id])
   end
 
   def update
-  
+    @mouse = Mouse.find(params[:id])
+    if @mouse.update(mouse_params)
+      redirect_to mouse_path(@mouse)
+    else
+      flash.now[:errors] = @mouse.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
-  
+    mouse = Mouse.find(params[:id])
+    mouse.destroy
+    redirect_to mice_url
+  end
+
+  private
+  def mouse_params
+    params.require(:mouse).permit(:name, :gold, :points)
   end
 end
