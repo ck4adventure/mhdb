19f40dec5dca9e46803e1fc671270ec960bf7166
Christina Kiesz <ckiesz@gmail.com>
2020-09-10 13:53:23 -0700

    working items form, create and edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,44 @@
 class ItemsController < ApplicationController
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      flash.now[:errors] = @item.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      flash.now[:errors] = @item.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :itype, :le)
+  end
 end
