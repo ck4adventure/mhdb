7ac766d77a9ea849f277345abdbace344fdce9c3
Christina Kiesz <ckiesz@gmail.com>
2020-09-10 12:21:36 -0700

    cleanup routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@
 #
 #                                Prefix Verb   URI Pattern                                                                              Controller#Action
 #                                  root GET    /                                                                                        static_pages#main
-#                                 users GET    /users(.:format)                                                                         users#index
-#                                       POST   /users(.:format)                                                                         users#create
+#                                 users POST   /users(.:format)                                                                         users#create
 #                              new_user GET    /users/new(.:format)                                                                     users#new
 #                             edit_user GET    /users/:id/edit(.:format)                                                                users#edit
 #                                  user GET    /users/:id(.:format)                                                                     users#show
@@ -17,9 +16,7 @@
 #                                  rank GET    /ranks/:id(.:format)                                                                     ranks#show
 #                               regions GET    /regions(.:format)                                                                       regions#index
 #                                region GET    /regions/:id(.:format)                                                                   regions#show
-#                         location_mice GET    /locations/:location_id/mouse_locations(.:format)                                        mouse_locations#index
-#                                       POST   /locations/:location_id/mouse_locations(.:format)                                        mouse_locations#create
-#                    new_location_mouse GET    /locations/:location_id/mouse_locations/new(.:format)                                    mouse_locations#new
+#                         location_mice POST   /locations/:location_id/mouse_locations(.:format)                                        mouse_locations#create
 #                        location_mouse DELETE /locations/:location_id/mouse_locations/:id(.:format)                                    mouse_locations#destroy
 #                             locations GET    /locations(.:format)                                                                     locations#index
 #                                       POST   /locations(.:format)                                                                     locations#create
@@ -29,9 +26,7 @@
 #                                       PATCH  /locations/:id(.:format)                                                                 locations#update
 #                                       PUT    /locations/:id(.:format)                                                                 locations#update
 #                                       DELETE /locations/:id(.:format)                                                                 locations#destroy
-#                       mouse_locations GET    /mouses/:mouse_id/mouse_locations(.:format)                                              mouse_locations#index
-#                                       POST   /mouses/:mouse_id/mouse_locations(.:format)                                              mouse_locations#create
-#                    new_mouse_location GET    /mouses/:mouse_id/mouse_locations/new(.:format)                                          mouse_locations#new
+#                       mouse_locations POST   /mouses/:mouse_id/mouse_locations(.:format)                                              mouse_locations#create
 #                        mouse_location DELETE /mouses/:mouse_id/mouse_locations/:id(.:format)                                          mouse_locations#destroy
 #                                  mice GET    /mouses(.:format)                                                                        mouses#index
 #                                       POST   /mouses(.:format)                                                                        mouses#create
@@ -41,20 +36,29 @@
 #                                       PATCH  /mouses/:id(.:format)                                                                    mouses#update
 #                                       PUT    /mouses/:id(.:format)                                                                    mouses#update
 #                                       DELETE /mouses/:id(.:format)                                                                    mouses#destroy
+#                                 items GET    /items(.:format)                                                                         items#index
+#                                       POST   /items(.:format)                                                                         items#create
+#                              new_item GET    /items/new(.:format)                                                                     items#new
+#                             edit_item GET    /items/:id/edit(.:format)                                                                items#edit
+#                                  item GET    /items/:id(.:format)                                                                     items#show
+#                                       PATCH  /items/:id(.:format)                                                                     items#update
+#                                       PUT    /items/:id(.:format)                                                                     items#update
+#                                       DELETE /items/:id(.:format)                                                                     items#destroy
 
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#main'
 
-  resources :users
+  resources :users, except: [:index]
   resource :session, only: [:new, :create, :destroy]
   resources :ranks, only: [:show, :index]
   resources :regions, only: [:show, :index]
   resources :locations do
-    resources :mouse_locations, as: 'mice', only: [:index, :new, :create, :destroy]
+    resources :mouse_locations, as: 'mice', only: [:create, :destroy]
   end
   resources :mouses, as: 'mice' do
-    resources :mouse_locations, as: 'locations', only: [:index, :new, :create, :destroy]
+    resources :mouse_locations, as: 'locations', only: [:create, :destroy]
   end
+  resources :items
 end
