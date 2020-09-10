fde99acb6b0376a0f79aec1a4b48b808f3bc8eca
Christina Kiesz <ckiesz@gmail.com>
2020-09-09 23:30:51 -0700

    adding mice locations works both ways

--- app/controllers/mouse_locations_controller.rb
+++ app/controllers/mouse_locations_controller.rb
@@ -5,13 +5,28 @@ class MouseLocationsController < ApplicationController
   end
   
   def create
-    @mouselocation = MouseLocation.new(mouse_id: params[:mouse_id])
-    @mouselocation.update(ml_params)
-    if @mouselocation.save
-      redirect_to edit_mouse_path(@mouselocation.mouse_id)
-    else
-      flash.now[:errors] = @mouselocation.errors.full_messages
-      redirect_to edit_mouse_path(@mouselocation.mouse_id)
+    if params[:location_id]
+      ml = MouseLocation.new(location_id: params[:location_id])
+      ml.update(ml_params)
+      if ml.save
+        redirect_to edit_location_path(ml.location_id)
+        return
+      else
+        flash.now[:errors] = ml.errors.full_messages
+        redirect_to edit_location_path(ml.location_id)
+        return
+      end
+    elsif params[:mouse_id]
+      ml = MouseLocation.new(mouse_id: params[:mouse_id])
+      ml.update(ml_params)
+      if ml.save
+        redirect_to edit_mouse_path(ml.mouse_id)
+        return
+      else
+        flash.now[:errors] = ml.errors.full_messages
+        redirect_to edit_mouse_path(ml.mouse_id)
+        return
+      end
     end
   end
 
@@ -29,7 +44,14 @@ class MouseLocationsController < ApplicationController
   def destroy
     ml = MouseLocation.find(params[:id])
     ml.destroy
-    redirect_to edit_mouse_path(ml.mouse_id)
+    if params[:location_id]
+      redirect_to edit_location_path(ml.location_id)
+      return
+    elsif params[:mouse_id]
+      redirect_to edit_mouse_path(ml.mouse_id)
+      return
+    end
+    redirect_to root_path
   end
 
   private
