37791a96830df4fb65f1f546d4a0d7981a905a2a
Christina Kiesz <ckiesz@gmail.com>
2020-09-11 21:36:25 -0700

    wip to get categories wired

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,10 @@
 class CategoriesController < ApplicationController
+  def index
 
+  end
+
+  def show
+    @cat = Category.find_by(name: params[:category])
+    @items = Items.find_by(category_id: @cat.id)
+  end
 end
