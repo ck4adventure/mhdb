39faed83b80703606dfb61cddb0c54ca734eaeeb
Christina Kiesz <ckiesz@gmail.com>
2020-09-11 23:55:22 -0700

    working categories list on items page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,4 @@
 class CategoriesController < ApplicationController
-  def index
-
-  end
-
   def show
     @category = Category.find_by(name: params[:category])
     @items = Item.find_by(category_id: @category.id)
