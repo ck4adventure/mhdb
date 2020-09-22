5845ec2d2f090d8e4abb82b5c96acd276234f2f7
Christina Kiesz <ckiesz@gmail.com>
2020-09-21 19:06:17 -0700

    create app and import to root

--- app/javascript/components/root.jsx
+++ app/javascript/components/root.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import App from './app'
 
 const Root = ({store}) => (
   <Provider store={store}>
-    <h2>Hi from Provider</h2>
+    <App />
   </Provider>
 )
 
