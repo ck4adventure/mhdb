82b68b1a11a829c47802fb8a856a7841419ff6a9
Christina Kiesz <ckiesz@gmail.com>
2020-09-21 19:21:39 -0700

    add skel ItemsIndex

---
+++ app/javascript/components/items/items_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux'
+
+import ItemsIndex from './items_index'
+
+const mapStateToProps = () => ({
+
+});
+
+const mapDispatchToProps = () => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemsIndex);
---
+++ app/javascript/components/items/items_index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class ItemsIndex extends React.Component {
+  render() {
+    return <div>Items Index</div>
+  }
+}
+
+export default ItemsIndex;
