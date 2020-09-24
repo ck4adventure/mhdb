0a4f0efbefd694974c6a627bca3e8a97c413a0b8
Christina Kiesz <ckiesz@gmail.com>
2020-09-23 19:18:24 -0700

    app thunks to store

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from '../reducers/root_reducer'
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState);
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 }
 
 export default configureStore;
