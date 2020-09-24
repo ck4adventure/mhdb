593afc2292636828dfc488c36ae2f7715ec4af08
Christina Kiesz <ckiesz@gmail.com>
2020-09-23 19:38:50 -0700

    split items and weapons down

--- app/javascript/actions/items_actions.js
+++ app/javascript/actions/items_actions.js
@@ -1,6 +1,9 @@
+import * as ItemsApiUtil from '../util/items_api';
+
 export const RECEIVE_ITEMS = "RECEIVE_ITEMS";
 export const RECEIVE_ITEM = "RECEIVE_ITEM";
 
+// sync actions
 export const receiveItems = (items) => {
   type: RECEIVE_ITEMS,
   items
@@ -10,4 +13,17 @@ export const receiveItem = (item) => {
   type: RECEIVE_ITEM,
   item
 }
+
+
+// async actions
+export const fetchItems = () => dispatch => (
+  ItemsApiUtil.fetchItems()
+    .then(
+      items => dispatch(receiveItems(items)),
+      )
+);
+
+export const fetchItem = () => dispatch => (
+  ItemsApiUtil.fetchItem().then(item => dispatch(receiveItem(item)))
+);
 
--- app/javascript/components/items/items_index.jsx
+++ app/javascript/components/items/items_index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 class ItemsIndex extends React.Component {
+  componentDidMount() {
+    this.props.requestWeapons();
+  }
+
   render() {
-    return <div>Items Index</div>
+    const { weapons } = this.props;
+
+    if (!weapons) { return <p>"Loading..."</p>; }
+    
+    return (
+      <div>
+        <h4>Weapons</h4>
+        <ul>
+          { this.props.weapons.forEach(wep => { <li>{wep.name}</li> }) }
+        </ul>
+      </div>
+    );
   }
 }
 
