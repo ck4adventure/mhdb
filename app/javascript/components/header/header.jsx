68225e7b55b36a8716cf51d1799861349197b87a
Christina Kiesz <ckiesz@gmail.com>
2020-09-25 13:54:08 -0700

    build out header mock

--- app/javascript/components/header/header.jsx
+++ app/javascript/components/header/header.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 class Header extends React.Component {
   render(){
-    return <h4>Mousehunt Collector</h4>;
+    return (
+      <div className="header__container"> 
+        <div className="header__container--left">
+          <div className="header wordmark">
+            MouseHunt Collector
+          </div>
+        </div>
+        <div className="header__container--right">
+          <div className="header user-info">
+            ck4adventure
+          </div>
+          <div className="header session-container">
+            Sign Out
+          </div>
+        </div>
+
+      </div>
+    );
   }
 }
 
 export default Header;
+
