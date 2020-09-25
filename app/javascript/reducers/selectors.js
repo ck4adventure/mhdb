ae0fc8b5b389c2fc0e1c0c84a5d7e881e30bf2ed
Christina Kiesz <ckiesz@gmail.com>
2020-09-24 17:15:41 -0700

    add filter byPowerType

--- app/javascript/reducers/selectors.js
+++ app/javascript/reducers/selectors.js
@@ -1,1 +1,15 @@
-export const toArray =  items  => Object.keys(items).map(key => items[key]);
+export const toArray =  items  => {
+  if (items) {
+    return Object.keys(items).map(key => items[key]);
+  } else {
+    return [];
+  }
+};
+
+export const byPowerType = (weapons, ptype) => {
+  if (weapons){
+    return Object.keys(weapons).filter(id => weapons[id].power_type == ptype).map(id => weapons[id]);
+  } else {
+    return [];
+  }
+}
