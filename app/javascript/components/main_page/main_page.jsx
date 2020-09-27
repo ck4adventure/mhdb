30fe9d570d73ad9743c47b78a676ad524d530e3a
Christina Kiesz <ckiesz@gmail.com>
2020-09-26 19:16:36 -0700

    updates

--- app/javascript/components/main_page/main_page.jsx
+++ app/javascript/components/main_page/main_page.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import ItemsIndex from '../items/items_container'
 import UserStatsBox from './user_stats_box'
-import WeaponsTabBox from './weapons_tab_box'
+import WeaponsTabBox from './weapons_tab_box_container'
 
 class MainPage extends React.Component {
+  componentDidMount() {
+    this.props.requestAllWeapons();
+    this.props.requestAllBases();
+  }
 
   render() {
-    const content = [{ title: "Arcane", power_type: "arcane" },{ title: "Physical", power_type: "physical" },{ title: "Rift", power_type: "rift" },];
+    const content = [{ title: "Physical", power_type: "physical" },{ title: "Arcane", power_type: "arcane" },{ title: "Rift", power_type: "rift" },];
 
     return (
       <div className="main_page__container">
         <UserStatsBox />
-        <WeaponsTabBox infos={content} />
+        <WeaponsTabBox panes={content} />
       </div>
     );
   }
--- app/javascript/components/main_page/user_stats_box.jsx
+++ app/javascript/components/main_page/user_stats_box.jsx
@@ -3,11 +3,61 @@ import React from 'react';
 const UserStatsBox = () => {
   return (
     <div className="userstats__container">
-      <div className="userstats__box">
+      <div className="userstats__greeting">
         <div>Welcome, Huntress</div>
-        <div>Stats are coming soon.</div>
+        <div>Current Rank: Grandmaster</div>
       </div>
-  </div>
+      <div className="userstats__section-header">
+        <div>Trap Collection</div>
+      </div>
+      <div className="userstats__weapons-stats">
+          <div className="userstats__item-box" >
+            <div>Arcane</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Draconic</div>
+            <div>13/15</div>
+          </div> 
+          <div className="userstats__item-box" >
+            <div>Forgotten</div>
+            <div>13/15</div>
+          </div>         
+          <div className="userstats__item-box" >
+            <div>Hydro</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Law</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Parental</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Physical</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Rift</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Tactical</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Shadow</div>
+            <div>13/15</div>
+          </div>
+          <div className="userstats__item-box" >
+            <div>Bases</div>
+            <div>100/155</div>
+          </div>
+
+      </div>
+    </div>
   )
 }
 
