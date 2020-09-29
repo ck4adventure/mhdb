affa3e0c6b5594cddbfb2fe407689da8e3b52fb0
Christina Kiesz <ckiesz@gmail.com>
2020-09-28 17:39:58 -0700

    Add material header and menu skel

--- app/javascript/components/header/header.jsx
+++ app/javascript/components/header/header.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Button, makeStyles } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class Header extends React.Component {
-  render(){
-    return (
-      <div id="header" className="header__container"> 
-        <div className="header__container--left">
-          <div className="header wordmark">
-            MouseHunt Collector
-          </div>
-        </div>
-        <div className="header__container--right">
-          <div className="header user-info">
-            ck4adventure
-          </div>
-          <div className="header session-container">
-            Sign Out
-          </div>
-        </div>
+import SimpleMenu from './simple_menu';
 
-      </div>
-    );
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          MouseHunt Collector
+        </Typography>
+        <SimpleMenu />
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
