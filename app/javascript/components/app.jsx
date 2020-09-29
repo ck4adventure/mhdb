9423d945e3184e8487feb777791903a5eaba54e6
Christina Kiesz <ckiesz@gmail.com>
2020-09-29 11:04:00 -0700

    initial custom app with imports

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,21 +1,15 @@
-import React from 'react'
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import CustomAppBar from './header/custom_app_bar';
+import CustomDrawer from './side_nav/custom_drawer';
 
-import Header from './header/header_container'
-import MainPage from './main_page/main_page_container'
-import SideNav from './side_nav/side_nav'
-import Footer from './footer/footer'
-
-const App = () => (
-  <div id="page-container" className="app">
-    <div id="content-wrap">
-      <Header />
-      <div id="content-body" className="app__main">
-        <SideNav />
-        <MainPage />
-     </div>
-     <Footer />
+export default function App() {
+  return (
+    <div className="appl">
+      <CssBaseline />
+      <CustomAppBar />
+      <CustomDrawer />
     </div>
-  </div>
-)
 
-export default App;
+  );
+}
