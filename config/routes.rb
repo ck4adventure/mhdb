f00b224b62d8780fd5c7cceffdb6151a82be8261
Christina Kiesz <ckiesz@gmail.com>
2020-10-07 15:32:39 -0700

    add session and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # == Route Map
 #
 #                                Prefix Verb   URI Pattern                                                                              Controller#Action
-#                                  root GET    /                                                                                        static_pages#main
+#                                  root GET    /                                                                                        static_pages#root
 #                                  main GET    /main(.:format)                                                                          static_pages#main
 #                                   app GET    /app(.:format)                                                                           static_pages#root
 #                     api_items_weapons GET    /api/items/weapons(.:format)                                                             api/items#weapons
@@ -10,6 +10,16 @@
 #                              api_item PATCH  /api/items/:id(.:format)                                                                 api/items#update
 #                                       PUT    /api/items/:id(.:format)                                                                 api/items#update
 #                                       DELETE /api/items/:id(.:format)                                                                 api/items#destroy
+#                             api_users POST   /api/users(.:format)                                                                     api/users#create
+#                          new_api_user GET    /api/users/new(.:format)                                                                 api/users#new
+#                         edit_api_user GET    /api/users/:id/edit(.:format)                                                            api/users#edit
+#                              api_user GET    /api/users/:id(.:format)                                                                 api/users#show
+#                                       PATCH  /api/users/:id(.:format)                                                                 api/users#update
+#                                       PUT    /api/users/:id(.:format)                                                                 api/users#update
+#                                       DELETE /api/users/:id(.:format)                                                                 api/users#destroy
+#                       new_api_session GET    /api/session/new(.:format)                                                               api/sessions#new
+#                           api_session DELETE /api/session(.:format)                                                                   api/sessions#destroy
+#                                       POST   /api/session(.:format)                                                                   api/sessions#create
 #                                 users POST   /users(.:format)                                                                         users#create
 #                              new_user GET    /users/new(.:format)                                                                     users#new
 #                             edit_user GET    /users/:id/edit(.:format)                                                                users#edit
@@ -53,6 +63,26 @@
 #                                       PATCH  /items/:id(.:format)                                                                     items#update
 #                                       PUT    /items/:id(.:format)                                                                     items#update
 #                                       DELETE /items/:id(.:format)                                                                     items#destroy
+#         rails_postmark_inbound_emails POST   /rails/action_mailbox/postmark/inbound_emails(.:format)                                  action_mailbox/ingresses/postmark/inbound_emails#create
+#            rails_relay_inbound_emails POST   /rails/action_mailbox/relay/inbound_emails(.:format)                                     action_mailbox/ingresses/relay/inbound_emails#create
+#         rails_sendgrid_inbound_emails POST   /rails/action_mailbox/sendgrid/inbound_emails(.:format)                                  action_mailbox/ingresses/sendgrid/inbound_emails#create
+#   rails_mandrill_inbound_health_check GET    /rails/action_mailbox/mandrill/inbound_emails(.:format)                                  action_mailbox/ingresses/mandrill/inbound_emails#health_check
+#         rails_mandrill_inbound_emails POST   /rails/action_mailbox/mandrill/inbound_emails(.:format)                                  action_mailbox/ingresses/mandrill/inbound_emails#create
+#          rails_mailgun_inbound_emails POST   /rails/action_mailbox/mailgun/inbound_emails/mime(.:format)                              action_mailbox/ingresses/mailgun/inbound_emails#create
+#        rails_conductor_inbound_emails GET    /rails/conductor/action_mailbox/inbound_emails(.:format)                                 rails/conductor/action_mailbox/inbound_emails#index
+#                                       POST   /rails/conductor/action_mailbox/inbound_emails(.:format)                                 rails/conductor/action_mailbox/inbound_emails#create
+#     new_rails_conductor_inbound_email GET    /rails/conductor/action_mailbox/inbound_emails/new(.:format)                             rails/conductor/action_mailbox/inbound_emails#new
+#    edit_rails_conductor_inbound_email GET    /rails/conductor/action_mailbox/inbound_emails/:id/edit(.:format)                        rails/conductor/action_mailbox/inbound_emails#edit
+#         rails_conductor_inbound_email GET    /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#show
+#                                       PATCH  /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#update
+#                                       PUT    /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#update
+#                                       DELETE /rails/conductor/action_mailbox/inbound_emails/:id(.:format)                             rails/conductor/action_mailbox/inbound_emails#destroy
+# rails_conductor_inbound_email_reroute POST   /rails/conductor/action_mailbox/:inbound_email_id/reroute(.:format)                      rails/conductor/action_mailbox/reroutes#create
+#                    rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+#             rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#                    rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+#             update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#                  rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -65,6 +95,8 @@ Rails.application.routes.draw do
     get 'items/weapons', to: 'items#weapons'
     get 'items/bases', to: 'items#bases'
     resources :items, only: [:create, :update, :destroy]
+    resources :users, except: [:index]
+    resource :session, only: [:create, :destroy]
 
   end
 
