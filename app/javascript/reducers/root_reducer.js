393f472d5d65e67a146652f3739955c2b99bac62
Christina Kiesz <ckiesz@gmail.com>
2020-10-08 11:07:10 -0700

    create ranks reducer

--- app/javascript/reducers/root_reducer.js
+++ app/javascript/reducers/root_reducer.js
@@ -4,10 +4,12 @@ import errorsReducer from './errors_reducer';
 import sessionReducer from './session_reducer';
 import usersReducer from './users_reducer';
 import uiReducer from './ui_reducer';
+import ranksReducer from './ranks_reducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   users: usersReducer,
+  ranks: ranksReducer,
   items: itemsReducer,
   errors: errorsReducer,
   ui: uiReducer,
