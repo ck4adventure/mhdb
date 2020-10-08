2f07bae2b38c4130e2d21b7d0f2a385d5b1ca4fa
Christina Kiesz <ckiesz@gmail.com>
2020-10-08 13:07:09 -0700

    add links from ranksindex

--- app/javascript/components/ranks/ranks_index.jsx
+++ app/javascript/components/ranks/ranks_index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { useHistory } from "react-router-dom";
 
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
 import { toArray } from '../../reducers/selectors';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,11 +19,25 @@ const useStyles = makeStyles((theme) => ({
 export default function RanksPage () {
   const r = useSelector(state => state.ranks);
   const classes = useStyles();
+  let history = useHistory();
 
+  const handlePath = (path, e) => {
+    e.preventDefault();
+    history.push(path);
+  }
   
-  const rarr = Object.keys(r).map(id => <li key={id}>{r[id].title}</li>);
+  const rarr = Object.keys(r).map(id => {
+    const rank = r[id];
+    return (
+      <li key={id}>
+        <Link href={`/ranks/${id}`} onClick={e => handlePath(`/ranks/${id}`, e)} underline="none">
+          {rank.title}
+        </Link>
+      </li>
+    )
+  });
   if (rarr.length == 0) {
-    return <h1>Ranks</h1>
+    return <h1>Loading</h1>
   }
   
   return (
