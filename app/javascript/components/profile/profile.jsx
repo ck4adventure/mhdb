74c079a4fa656f946cf8a46d0e2e0e2c31231c74
Christina Kiesz <ckiesz@gmail.com>
2020-10-09 14:48:33 -0700

    bugfix: profile rank link

--- app/javascript/components/profile/profile.jsx
+++ app/javascript/components/profile/profile.jsx
@@ -1,9 +1,16 @@
 import { Link } from '@material-ui/core';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux'
 
 export default function Profile() {
   const session = useSelector(state => state.session)
+  let history = useHistory();
+
+  function handlePath(path, event) {
+    event.preventDefault();
+    history.push(path);
+  }
 
   if (!Boolean(session)) {
     return <h1>Loading...</h1>
