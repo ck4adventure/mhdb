52caa8c517ed7ef7caa3dfddb5cdc44432f3c3e7
Christina Kiesz <ckiesz@gmail.com>
2020-10-12 16:59:08 -0700

    create ajax post

--- app/javascript/util/locations_api.js
+++ app/javascript/util/locations_api.js
@@ -2,3 +2,9 @@ export const fetchLocations = () => $.ajax({
   type: "GET",
   url: "api/locations",
 })
+
+export const postLocation = (location) => $.ajax({
+  type: "POST",
+  url: "api/locations",
+  data: { location },
+})
