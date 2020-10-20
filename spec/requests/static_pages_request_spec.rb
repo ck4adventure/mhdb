c4e580d4c2b586a3bf265b5b3f578e9fb2ce63c3
Christina Kiesz <ckiesz@gmail.com>
2020-10-20 15:11:56 -0700

    pass static pages checks

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe '#main' do
-    it 'returns the main page' do
-      get "/"
+    it 'returns the main rails view page' do
+      get "/main"
       expect(response).to render_template(:main)
       expect(response).to have_http_status(:ok)
     end
