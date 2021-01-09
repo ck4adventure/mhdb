62fe873b9e81f050e42278e39ccaa42b9e30532e
Christina Kiesz <ckiesz@gmail.com>
2021-01-08 17:24:18 -0800

    split seeds

---
+++ db/seeds/008_cheeses.rb
@@ -0,0 +1,40 @@
+# Bait
+cheeses = [
+  {
+    name: "Cheddar",
+    itype: "bait",
+  },
+  {
+    name: "Marble",
+    itype: "bait",
+  },
+  {
+    name: "Swiss",
+    itype: "bait",
+  },
+  {
+    name: "Brie",
+    itype: "bait"
+  },
+  {
+    name: "Gouda",
+    itype: "bait"
+  },
+    {
+    name: "Marble String",
+    itype: "bait",
+  },
+  {
+    name: "Swiss String",
+    itype: "bait"
+  },
+  {
+    name: "Brie String",
+    itype: "bait"
+  },
+
+]
+
+cheeses.each do |ch|
+  bait = Item.find_or_create_by(ch)
+end
