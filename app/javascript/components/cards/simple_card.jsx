d4f7080e6ffd5d9c9fd75b34fea41bde15898446
Christina Kiesz <ckiesz@gmail.com>
2021-01-16 15:43:04 -0800

    change main page boxes bigger

--- app/javascript/components/cards/simple_card.jsx
+++ app/javascript/components/cards/simple_card.jsx
@@ -1,68 +1,62 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { CardMedia } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { CardMedia } from "@material-ui/core";
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   croot: {
-    minWidth: 75,
+    minWidth: 100,
     margin: 10,
-    display: 'flex',
-    flexDirection: 'column',
-
+    display: "flex",
+    flexDirection: "column",
   },
   content: {
     padding: 0,
     margin: 4,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 0,
   },
   number_box: {
     marginLeft: 6,
-    fontSize: 14,
+    fontSize: 20,
     color: theme.palette.primary.main,
-
   },
   media: {
-    height: 20,
-    width: 20,
+    height: 40,
+    width: 40,
   },
   button_box: {
     padding: 0,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   button_text: {
     fontSize: 11,
-    color: 'gray',
-  }
+    color: "gray",
+  },
 }));
 
 export default function SimpleCard(props) {
   const classes = useStyles();
 
-
   return (
     <Card className={classes.croot} variant="outlined">
       <CardContent className={classes.content}>
-        <CardMedia        
-            className={classes.media}
-            image={props.img}
-            title={props.name}
+        <CardMedia
+          className={classes.media}
+          image={props.img}
+          title={props.name}
         />
-        <Typography className={classes.number_box}>
-          {props.count}
-        </Typography>
+        <Typography className={classes.number_box}>{props.count}</Typography>
       </CardContent>
-      <CardActions className={classes.button_box}>
+      {/* <CardActions className={classes.button_box}>
         <Button size="small" className={classes.button_text}>{props.name}</Button>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 }
--- app/javascript/components/main_page/main.jsx
+++ app/javascript/components/main_page/main.jsx
@@ -1,82 +1,98 @@
-import React  from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import Container from '@material-ui/core/Container';
-import SimpleCard from '../cards/simple_card';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import Container from "@material-ui/core/Container";
+import SimpleCard from "../cards/simple_card";
 
-const images = require.context('../../images', true)
-const imagePath = (name) => images(name, true)
+const images = require.context("../../images", true);
+const imagePath = (name) => images(name, true);
 
-import Traps from '../../images/traps.png';
-import Collectibles from '../../images/collectibles.png';
-import Travel from '../../images/travel.png';
+import Traps from "../../images/traps.png";
+import Collectibles from "../../images/collectibles.png";
+import Travel from "../../images/travel.png";
+import Recruit from "../../images/recruit.gif";
+import Mice from "../../images/mice.png";
 
-
-import { toArray } from '../../reducers/selectors';
-
+import { toArray } from "../../reducers/selectors";
 
 const useStyles = makeStyles((theme) => ({
-  stats_container:{
-    display: 'flex',
-    justifyContent: 'center',
-
+  stats_container: {
+    display: "flex",
+    justifyContent: "center",
   },
   weapons_box: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
     maxWidth: 530,
     marginBottom: 20,
     marginRight: 20,
-
   },
   single_box: {
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
     // marginLeft: 20,
     marginBottom: 20,
     marginRight: 20,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   box_title: {
     marginTop: 10,
     marginLeft: 10,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    alignContent: 'flex-end',
+    alignContent: "flex-end",
   },
   cards_container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
 }));
 
-const plist = ["arcane", "draconic", "forgotten", "hydro", "law", "parental", "physical", "rift", "shadow", "tactical"];
+const plist = [
+  "arcane",
+  "draconic",
+  "forgotten",
+  "hydro",
+  "law",
+  "parental",
+  "physical",
+  "rift",
+  "shadow",
+  "tactical",
+];
 
-const cardsForList = plist.map(el => <SimpleCard name={el} key={el} img={imagePath('./' + `${el}`)} count={10} />);
+const cardsForList = plist.map((el) => (
+  <SimpleCard name={el} key={el} img={imagePath("./" + `${el}`)} count={10} />
+));
 
-export default function Main () {
+export default function Main() {
   const classes = useStyles();
-  const bases = useSelector(state => state.items.bases ? toArray(state.items.bases) : []);
 
+  const ranks = useSelector((state) =>
+    state.ranks ? toArray(state.ranks) : []
+  );
 
   return (
     <div>
       <div className={classes.drawerHeader} />
       <Container className={classes.stats_container}>
-        
-        <Paper elevation={8} className={classes.weapons_box} >
-          <Typography variant="h6" component="div" className={classes.box_title}>
+        <Paper elevation={8} className={classes.weapons_box}>
+          <Typography
+            variant="h6"
+            component="div"
+            className={classes.box_title}
+          >
             Weapons
           </Typography>
           <Divider />
@@ -86,41 +102,53 @@ export default function Main () {
         </Paper>
       </Container>
       <Container className={classes.stats_container}>
-        
         <Paper elevation={8} className={classes.single_box}>
-          <Typography variant="h6" component="div" className={classes.box_title}>
-            Bases
+          <Typography
+            variant="h6"
+            component="div"
+            className={classes.box_title}
+          >
+            Ranks
           </Typography>
           <Divider />
           <Container className={classes.cards_container}>
-            <SimpleCard name={"bases"} key={"bases"} img={Traps} count={bases.length}/>
+            <SimpleCard
+              name={"ranks"}
+              key={"ranks"}
+              img={Recruit}
+              count={ranks.length}
+            />
           </Container>
         </Paper>
 
         <Paper elevation={8} className={classes.single_box}>
-          <Typography variant="h6" component="div" className={classes.box_title}>
-            Collectibles
+          <Typography
+            variant="h6"
+            component="div"
+            className={classes.box_title}
+          >
+            Groups
           </Typography>
           <Divider />
           <Container className={classes.cards_container}>
-            <SimpleCard name={"collectibles"} key={"collectibles"} img={Collectibles} />
+            <SimpleCard name={"groups"} key={"groups"} img={Mice} />
           </Container>
         </Paper>
 
         <Paper elevation={8} className={classes.single_box}>
-          <Typography variant="h6" component="div" className={classes.box_title}>
+          <Typography
+            variant="h6"
+            component="div"
+            className={classes.box_title}
+          >
             Locations
           </Typography>
           <Divider />
           <Container className={classes.cards_container}>
-            <SimpleCard name={"locations"} key={"locations"} img={Travel}/>
+            <SimpleCard name={"locations"} key={"locations"} img={Travel} />
           </Container>
         </Paper>
-      
-      
-      
       </Container>
     </div>
   );
-
 }
